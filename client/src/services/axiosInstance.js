5c9776b4c456d680067ac564122a70d5ba4f6403
Zafar Ekhlaque <Zafarekhlaque@gmail.com>
2024-09-28 20:00:29 +0530

    env change

--- client/src/services/axiosInstance.js
+++ client/src/services/axiosInstance.js
@@ -7,7 +7,7 @@ const axiosInstance = axios.create({
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
-    'Authorization':apiKey
+    'Authorization': "Bearer 4ApVMIn5sTxeW7GQ5VWeWiy"
   },
 });
 
